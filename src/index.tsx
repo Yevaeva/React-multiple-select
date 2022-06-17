c0fa39a5be92ee3c04779cae3ca4e163dc5dd869
Bee Web <beeweb.everse.1@Bees-MacBook-Pro.local>
2022-06-17 12:12:49 +0400

    added basename to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter basename="/React-multiple-select">
+  <BrowserRouter
+    basename={
+      process.env.NODE_ENV === "development" ? "" : "/React-multiple-select"
+    }
+  >
     <App />
   </BrowserRouter>
 );
